c691a95cfa0daedf4416e4ab24e6fa7eac8ebb3d
RPleasnick <rpleasni@gmail.com>
2024-06-05 17:22:25 -0400

    init complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,19 @@
 // Build the metadata panel
 function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
+    console.log(sample)
     // get the metadata field
-
+    let metadataField = data.metadata;
 
     // Filter the metadata for the object with the desired sample number
-
+    let metadataNum = metadataField.filter(item => item.id == sample)[0];
+    console.log(metadataNum);
 
     // Use d3 to select the panel with id of `#sample-metadata`
-
+    let panel = d3.select('#sample-metadata');
 
     // Use `.html("") to clear any existing metadata
-
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
@@ -54,30 +55,36 @@ function buildCharts(sample) {
 // Function to run on page load
 function init() {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
-
+    console.log(data);
     // Get the names field
-
+    let names = data.names;
+    console.log(names);
 
     // Use d3 to select the dropdown with id of `#selDataset`
-
+    let dropdown = d3.select('#selDataset');
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
+    for (let i = 0; i < names.length; i++) {
+      dropdown.append("option").text(names[i]).property("value", names[i]);
 
+    }
 
     // Get the first sample from the list
-
+    let first = names[0];
 
     // Build charts and metadata panel with the first sample
-
+    buildCharts(first);
+    buildMetadata(first);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
-
+  buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialize the dashboard
